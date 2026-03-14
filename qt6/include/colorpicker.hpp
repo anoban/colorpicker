4b82bd916082d73fc359356dc1f2ef1e6cd2fada
anoban <2016sb117@univ.jfn.ac.lk>
2026-03-15 09:38:53 +1100

    modified:   qt6/.clang-format 	modified:   qt6/.clang-tidy 	modified:   qt6/.clangd 	modified:   qt6/include/colorpicker.hpp 	new file:   qt6/include/config.hpp

--- qt6/include/colorpicker.hpp
+++ qt6/include/colorpicker.hpp
@@ -1,27 +1,7 @@
 #pragma once
-#define __COLORPICKER_H
-
-#include <limits.h>
-
-#define MAINWINDOW_HEIGHT            330 // height of the application window
-#define MAINWINDOW_WIDTH             610 // width of the application window
-
-#define VSPACE_TRACKBARS             40 // vertical separation between subsequent track bars
-#define NTRACKBARS                   3  // number of track bars used in the application
-
-#define TRACKBAR_WIDTH               260
-#define TRACKBAR_HEIGHT              25
-#define TRACKBAR_LEFTPAD             20  // padding between the left end of the track bars and the left edge of the client window
-#define TRACKBARGRID_VERTICAL_MARGIN 150 // margin betwen the first track bar and the bottom edge of the title bar
-
-#define TRACKBAR_LABEL_LEFTPAD       20 // padding between the right end of the track bar and the left edge of it's cognate label box
-#define TRACKBAR_LABEL_WIDTH         50
-#define TRACKBAR_LABEL_HEIGHT        30
-
-#define HEXSTRING_SIZE               20 // max length of the hexadecimal colour code string e.g. "#00AEEF"
-#define HEXSTRING_TEXTBOX_WIDTH      120
-#define HEXSTRING_TEXTBOX_HEIGHT     30
-
-#define PAGE_UPDOWN_STEP             17 // distance to move the slider on the track bar, when page-up or page-down keys were pressed
-
-#define APPLICATION_TITLE            "Colorpicker"
+#ifndef __COLORPICKER_HPP
+    #define __COLORPICKER_HPP           1
+    #define __COLORPICKER_VERSION_MAJOR 0
+    #define __COLORPICKER_VERSION_MINOR 0.1
+    #define __COLORPICKER_VERSION       ((__COLORPICKER_VERSION_MAJOR + __COLORPICKER_VERSION_MINOR))
+#endif
---
+++ qt6/include/config.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef __CONFIG_HPP
+    #define __CONFIG_HPP 1
+#endif
+
+static constexpr unsigned long MAINWINDOW_HEIGHT { 330 }; // height of the application window
+static constexpr unsigned long MAINWINDOW_WIDTH { 610 };  // width of the application window
+
+#define VSPACE_TRACKBARS 40 // vertical separation between subsequent track bars
+#define NTRACKBARS       3  // number of track bars used in the application
+
+static constexpr unsigned long TRACKBAR_WIDTH { 260 };
+static constexpr unsigned long TRACKBAR_HEIGHT { 25 };
+static constexpr unsigned long TRACKBAR_LEFTPAD { 20 };              // padding between the left end of the track bars and the left edge of the client window
+static constexpr unsigned long TRACKBARGRID_VERTICAL_MARGIN { 150 }; // margin betwen the first track bar and the bottom edge of the title bar
+
+static constexpr unsigned long TRACKBAR_LABEL_LEFTPAD { 20 }; // padding between the right end of the track bar and the left edge of it's cognate label box
+static constexpr unsigned long TRACKBAR_LABEL_WIDTH { 50 };
+static constexpr unsigned long TRACKBAR_LABEL_HEIGHT { 30 };
+
+static constexpr unsigned long HEXSTRING_SIZE { 20 }; // max length of the hexadecimal colour code string e.g. "#00AEEF"
+static constexpr unsigned long HEXSTRING_TEXTBOX_WIDTH { 120 };
+static constexpr unsigned long HEXSTRING_TEXTBOX_HEIGHT { 30 };
+
+#define PAGE_UPDOWN_STEP  17 // distance to move the slider on the track bar, when page-up or page-down keys were pressed
+
+#define APPLICATION_TITLE "Colorpicker"
