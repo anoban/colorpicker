b77b4151be96fb9817d083592e1f0f0c03a61118
anoban <2016sb117@univ.jfn.ac.lk>
2026-03-19 09:45:16 +1100

    modified:   qt6/include/rgbhexstring.hpp

--- qt6/include/rgbhexstring.hpp
+++ qt6/include/rgbhexstring.hpp
@@ -17,15 +17,22 @@ class rgbhexstring final : public QLineEdit {
         Q_OBJECT
 
     private:
-        int     _rslider_value;
-        int     _gslider_value;
-        int     _bslider_value;
-        QString _hexstring; // QString is equivalent to std::wstring on Windows where wchar_t is 16 bits wide
+        int         _rslider_value;
+        int         _gslider_value;
+        int         _bslider_value;
+        QString     _hexstring; // QString is equivalent to std::wstring on Windows where wchar_t is 16 bits wide
+        QTextStream _hexstrstream;
 
     public:
         inline rgbhexstring(QWidget* const _parent_window = nullptr) noexcept :
-            QLineEdit(_parent_window), _rslider_value {}, _gslider_value {}, _bslider_value {}, _hexstring {} {
+            QLineEdit(_parent_window), _rslider_value {}, _gslider_value {}, _bslider_value {}, _hexstring {}, _hexstrstream { &_hexstring } {
             _hexstring.resize(configs::hexstring::SIZE);
+            _hexstrstream.setPadChar('0');                            // pad the hex representation with zeroes to make it two digits when the value is < 16
+            _hexstrstream.setFieldAlignment(QTextStream::AlignRight); // when we only have one digit, pad with a zero on the left
+            _hexstrstream << Qt::hex                                  // we want the hex representation
+                          << Qt::uppercasedigits                      // we want the hex representations in upper case
+                          << ::qSetFieldWidth(2);                     // we want fixed width of 2 characters
+
             setGeometry( // since we have control over this class's implementation, let's do this inside the ctor, instead of having main window do this
                 configs::trackbars::PAD + configs::trackbars::WIDTH + configs::trackbars::labels::PAD + configs::trackbars::labels::WIDTH +
                     configs::trackbars::labels::PAD,
@@ -56,19 +63,16 @@ class rgbhexstring final : public QLineEdit {
             // static_assert(sizeof(QChar) == sizeof(wchar_t));                   // QChar is equivalent to unsigned short
             // the above requires -fshort-wchar compiler flag because by default, wchar_t is 4 bytes on linux??? not 2 bytes
             ::memset(_hexstring.data(), 0, sizeof(QChar) * _hexstring.size()); // clean up the buffer before every write
+            if (!_hexstrstream.seek(0)) {
+                fprintf(stderr, "Error inside %s, call to QTextStream::seek() failed!\n", __PRETTY_FUNCTION__);
+                return;
+            }
 
             // https://stackoverflow.com/questions/4784155/how-to-format-a-qstring
             // even if we use two byte wchar_t s, we don't know that there's alternative implementations for all the stdio.h functions to handle 2 byte wchar_t s????
             // https://doc.qt.io/qt-6/qstring.html#asprintf QString::asprintf isn't recommended in new Qt code :(
 
-            auto sstream = QTextStream(&_hexstring);
-            sstream.setPadChar('0');                            // pad the hex representation with zeroes to make it two digits when the value is < 16
-            sstream.setFieldAlignment(QTextStream::AlignRight); // when we only have one digit, pad with a zero on the left
-
-            sstream << '#' << Qt::hex      // we want the hex representation
-                    << Qt::uppercasedigits // we want the hex representations in upper case
-                    << ::qSetFieldWidth(2) // we want fixed width of 2 characters
-                    << _rslider_value << _gslider_value << _bslider_value;
+            _hexstrstream << '#' << _rslider_value << _gslider_value << _bslider_value;
 
             // setText(QString("#%1%2%3").arg(_rslider_value, 2, 16, u'0').arg(_gslider_value, 2, 16, u'0').arg(_bslider_value, 2, 16, u'0'));
             // ::puts(_hexstring.toStdString().c_str());
